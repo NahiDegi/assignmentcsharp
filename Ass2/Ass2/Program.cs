6c51db4c7e413cd8c552758b54f49b99c1ad17b5
lmilindj <milindlalwani1997@gmail.com>
2018-05-26 14:05:09 +0530

    Q1-5,8.

--- Ass2/Ass2/Program.cs
+++ Ass2/Ass2/Program.cs
@@ -10,6 +10,30 @@ namespace Ass2
     {
         static void Main(string[] args)
         {
+            //Q3
+            Portfolio portfolio = new Portfolio();
+            Random random = new Random();
+
+
+            //Q4
+            for (int i = 0; i < 5; i++)
+            {
+                portfolio.AddToPortfolio(new Stock("ID" + i,
+                    random.Next(100, 200),
+                    random.Next(1, 10)));
+            }
+
+            //Testing stocks use ctrl + k,u to uncomment
+            foreach (Stock stock in portfolio.CollectionOfStocks)
+            {
+                Console.WriteLine(stock.ToString());
+            }
+
+
+            //Q5
+            Console.WriteLine("Total Notional of Portfolio : "+portfolio.GetNotional());
+
+
         }
     }
 }
---
+++ Ass2/Ass2/Stock.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Q1.
+
+namespace Ass2
+{
+    class Stock
+    {
+        public string ID { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Stock(string iD, double price, int quantity)
+        {
+            ID = iD;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double GetNotional()
+        {
+            return Price * Quantity;
+        }
+
+        public void SplitStocks()
+        {
+            Price /= 2;
+            Quantity *= 2;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("ID : {0}, Price : {1}, Quantity : {2}",ID,Price,Quantity);
+        }
+
+    }
+}
