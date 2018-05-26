a53da210d7a16660d3cc3afd4806c80646f3ca4a
lmilindj <milindlalwani1997@gmail.com>
2018-05-26 19:36:33 +0530

    Q1-2

--- Ass4/Ass4/Program.cs
+++ Ass4/Ass4/Program.cs
@@ -13,77 +13,15 @@ namespace Ass4
 
         static void Main(string[] args)
         {
-            //Q3
-            Portfolio portfolio = new Portfolio();
-            Random random = new Random();
+            //4Q1
 
-            //3Q8
-            portfolio.Subscribe();
+            Stock s1 = new Stock("Rel", 56, 4);
+            Stock s2 = new Stock("Rel", 56, 4);
 
+            //4Q2
 
-            //3Q7
-            for (int i = 0; i < 5; i++)
-            {
-                portfolio.AddToPortfolio(new Stock("StockID" + i,
-                    random.Next(100, 200),
-                    random.Next(1, 10)));
-            }
+            Console.WriteLine(s1.Equals(s2));
 
-            for (int i = 0; i < 5; i++)
-            {
-                portfolio.AddToPortfolio(new Bond("BondID" + i,
-                    random.Next(100, 200),
-                    random.Next(1, 10),
-                    random.Next(1,10),
-                    random.Next(1,5)));
-            }
-
-            //Testing products use ctrl + k,u to uncomment
-            //foreach (Product stock in portfolio.CollectionOfStocks)
-            //{
-            //    Console.WriteLine(stock.ToString());
-            //}
-
-            //3Q9
-            Console.WriteLine("Total Notional of Portfolio : " + portfolio.GetNotional() + "\n");
-
-            //3Q10
-            foreach (Product product in portfolio.CollectionOfStocks)
-            {
-                if (product.ToString().Equals("Stock"))
-                {
-                    Console.WriteLine("Stock ID : {0}, Quantity : {1}, Price : {2}", 
-                        product.ID, 
-                        product.Quantity, 
-                        product.Price);
-                }
-            }
-
-
-            //3Q11
-            portfolio.Call("Chill");
-
-            //3Q13
-            foreach (Product product in portfolio.CollectionOfStocks)
-            {
-                if (product.ToString().Equals("Bond"))
-                {
-                    Console.WriteLine("Bond coupon : {0}",((Bond)product).GetCoupon());
-                }
-            }
-
-            //printProduct(portfolio.CollectionOfStocks.ToList());
-
-            //3Q14
-            printProduct(portfolio.CollectionOfStocks.ToList().Where(r => r.GetNotional()>1000));
-        }
-
-        private static void printProduct(IEnumerable<Product> enumerable)
-        {
-            foreach (Product product in enumerable)
-            {
-                Console.WriteLine("ID : {0}, Notional : {1}",product.ID,product.GetNotional());
-            }
         }
     }
 }
